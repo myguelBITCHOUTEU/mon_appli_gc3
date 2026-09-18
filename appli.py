07034e6701c5d87fab059202d2339c058e685bf4
myguelBITCHOUTEU <bitchouteumyguel21@gmail.com>
2026-09-18 10:06:27 +0100

    Update appli.py

--- appli.py
+++ appli.py
@@ -43,7 +43,7 @@ with st.sidebar.form("form_dechet"):
 	cause=st.text_input("cause")
 	unite=st.selectbox("Unite",["Unite (U)","kg","m3","Tonne"])
 
-	submit=st.form_submit_button("calculer et enregistrer le dechet")
+	submit=st.form_submit_button("calculer la perte et enregistrer le dechet")
 
 if submit:
 	cout= quantite*PRIX_UNITAIRES[type_dechet]
@@ -52,7 +52,7 @@ if submit:
 	df=load_data()
 	df=pd.concat([df,new_row],ignore_index=True)
 	df.to_csv(DATA_FILE,index=False)
-	st.sidebar.success(f"Perte enregistree : {cout} FCFA")
+	st.sidebar.success(f"Perte enregistrée : {cout} FCFA")
 
 #------------------- ANALYSE DES DONNEES ----------------
 
@@ -73,7 +73,7 @@ if not df.empty:
 		st.subheader("Evolution temporelle")
 		df["Date"]=pd.to_datetime(df["Date"])
 		fig_line=px.line(df.sort_values("Date"),x="Date",y="Quantite",color="Type")
-		fig_pie=px.pie(df,names="Type",values="Cout_Estime",title="Repartition de la perte (%)")
+		fig_pie=px.pie(df,names="Type",values="Cout_Estime",title="Repartition des pertes (%)")
 		st.plotly_chart(fig_line,use_container_width=True)
 		st.plotly_chart(fig_pie)
 
@@ -85,7 +85,7 @@ if not df.empty:
 	csv=df.to_csv(index=False).encode("utf-8")
 	st.download_button("Telecharger le rapport CSV",csv,"rapport_chantier.csv","text/csv")
 else:
-	st.info("Aucune donnee disponible. Utilisez le formulaire a gauche pour commencer.")
+	st.info("Aucune donnée disponible. Utilisez le formulaire à gauche pour commencer.")
 	
 		
 	
